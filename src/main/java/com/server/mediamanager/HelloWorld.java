8af9de145413011b85fff0b2fc51b2432a6ae61e
lijie <jack68@@>
2022-06-27 21:57:51 +0800

    jni

---
+++ src/main/java/com/server/mediamanager/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.server.mediamanager;
+
+class HelloWorld {
+     static {
+         System.loadLibrary("HelloWorld");
+     }
+    public native void print();
+}
---
+++ src/main/java/com/server/mediamanager/MediamanagerApplication.java
@@ -0,0 +1,37 @@
+package com.server.mediamanager;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.nativex.hint.NativeHint;
+
+@SpringBootApplication
+//@EnableOpenApi
+@Slf4j
+
+@NativeHint(options = {
+		"-H:+ReportExceptionStackTraces",
+		"-H:+ReportExceptionStackTraces",
+		"-H:ConfigurationFileDirectories=config-dir",
+		"-H:TraceClassInitialization=true",
+		"-H:+AllowVMInspection",
+		"-H:+JNI"
+})
+public class MediamanagerApplication {
+
+
+
+	public static void main(String[] args) {
+		try{
+			log.info("Media Manager Service starting.");
+			log.info("Media Manager Service starting===" + System.getProperty("java.library.path"));
+			ConfigurableApplicationContext mContext = SpringApplication.run(MediamanagerApplication.class, args);
+			new HelloWorld().print();
+		}catch (Exception e) {
+			log.error("start MediamanagerApplication error", e);
+		}
+
+	}
+
+}
